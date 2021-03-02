92a15e71e58389a02574102b304c5bbbfdfc533a
Kavindu Sanjula Dasanayake <41934090+KavinduSanjula@users.noreply.github.com>
2021-03-02 16:19:29 +0530

    Update settings.py

--- FactoryManagementSystem/FactoryManagementSystem/settings.py
+++ FactoryManagementSystem/FactoryManagementSystem/settings.py
@@ -21,7 +21,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '9d^br^_17htf@_g$$f&c7a-w#r0+4*xcxj4q*!8jl1yo(gp^jp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
